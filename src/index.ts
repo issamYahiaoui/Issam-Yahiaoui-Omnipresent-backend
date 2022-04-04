c1e35753f7c8b55f93afe1ff7b06eca81828f7bc
issam yahiaoui <issam.y@rizek.com>
2022-04-04 14:02:03 +0400

    Fix linter ...

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import helmet from 'helmet'
 import { StartProjectInit } from '@tsclean/core'
 import { AppContainer } from '@/application/app'
 import { PORT } from '@/application/config/environment'
+import { loadersInit } from "@/infrastructure/loaders";
 
 async function run (): Promise<void> {
-  // await loadersInit()
+  await loadersInit()
   const app = await StartProjectInit.create(AppContainer)
   app.use(helmet())
   await app.listen(PORT, () => console.log('Running on port ' + PORT))
